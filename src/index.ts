1d3bd10d1c85c91c8b6ece54fa52f78887aff974
ALPER TUNGA <alper@altu.dev>
2024-08-03 20:41:25 +0300

    [Feature]: JSON File reader.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import {serve} from '@hono/node-server'
+import {Hono} from 'hono'
+import ollama from 'ollama';
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+    return c.text('Hello Hono!')
 })
 
-app.post('/generate', (c) => {
-  return c.json({ message: 'Generate' });
+const messageHistory = [
+    {role: 'system', content: `You're a Turkish speaking assistant! Please write your responses in Turkish.`},
+]
+
+app.post('/generate', async (c) => {
+
+    const body = await c.req.json();
+
+    const prompt = body.prompt;
+
+    messageHistory.push( {role: 'user', content: prompt});
+
+    const response = await ollama.chat({
+        model: 'gemma2:latest', // siz lutfen gemma2:2b modelini kullanin
+        messages:messageHistory,
+    })
+
+    console.log(messageHistory);
+
+    messageHistory.push( {role: 'assistant', content: response.message.content});
+
+    return c.json({message: response.message.content});
 })
 
 const port = 3002
 console.log(`Server is running on port ${port}`)
 
 serve({
-  fetch: app.fetch,
-  port
+    fetch: app.fetch,
+    port
 })
